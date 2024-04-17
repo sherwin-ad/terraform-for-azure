408096c8bbf67c2ba88270bc8a5e336c8b290391
sherwin-ad <sherwin_ad@yahoo.com>
2024-04-17 08:53:57 +0800

    updated

--- HelloWorld/main.tf
+++ HelloWorld/main.tf
@@ -1,12 +1,18 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "3.99.0"
     }
   }
 }
 
 provider "azurerm" {
-  # Configuration options
+  features { 
+  }
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example"
+  location = "Eastus"
 }
